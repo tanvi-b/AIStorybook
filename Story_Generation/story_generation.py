e7702dc1831f2ecd0064c76d443c0e94f88039d1
tanvi-b <bhattadtanvi@gmail.com>
2025-01-13 21:35:48 -0600

    -adding loading spinner, clearing book, adding details to prompt

--- Story_Generation/story_generation.py
+++ Story_Generation/story_generation.py
@@ -9,9 +9,9 @@ from PIL import Image
 
 TEXT_API_URL = "https://api-inference.huggingface.co/models/Qwen/Qwen2.5-Coder-32B-Instruct"
 IMAGE_API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-3-medium-diffusers"
-headers = {"Authorization": f"Bearer hf_(token)"}
+headers = {"Authorization": f"Bearer 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"}
 
-def generate_text(prompt, max_pages=13, tokens_per_page=100):
+def generate_text(prompt, max_pages=20, tokens_per_page=100):
     story = prompt.strip()
     story_pages = []
 
